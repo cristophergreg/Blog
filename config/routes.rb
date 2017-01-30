dbc6c497815b4207f2d16c9c884ec23e23b89e6f
Cristopher <cristophergreg@gmail.com>
2017-01-30 17:56:08 -0500

    Add route new for page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/pages', to: 'pages#index'
-  get 'pages/:id', to: 'pages#show', as: 'page'
+  get '/pages/new', to: 'pages#new'
+  get '/pages/:id', to: 'pages#show', as: 'page'
+  
 end
