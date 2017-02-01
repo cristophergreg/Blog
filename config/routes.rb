5ea0d142f5aa7ddcc5c1a64caf16d67bf5ee8b55
Cristopher <cristophergreg@gmail.com>
2017-02-01 16:41:15 -0500

    Add view new for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/pages', to: 'pages#index'
-  get '/pages/new', to: 'pages#new'
+  get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
-  
+
 end
